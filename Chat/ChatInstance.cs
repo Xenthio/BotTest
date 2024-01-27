cba718cc65114e90a649092efa992adfa6f78fe3
Xenthio <thegooster88@gmail.com>
2024-01-28 09:38:07 +1100

    personality boilerplate

--- Chat/ChatInstance.cs
+++ Chat/ChatInstance.cs
@@ -4,4 +4,17 @@ public class ChatInstance
 {
 	public ITextgenAPI API;
 	public List<ChatMessage> Messages = new List<ChatMessage>();
+
+	public void SendMessage(ChatMessage message)
+	{
+
+	}
+	public ChatMessage ReceiveMessage(ChatPersona personality)
+	{
+		var msg = new ChatMessage()
+		{
+			Sender = ""
+		};
+		return msg;
+	}
 }
---
+++ Chat/ChatPersona.cs
@@ -0,0 +1,9 @@
+﻿namespace BotTest;
+
+// the character the LLM should pretend to be.
+public class ChatPersona
+{
+	public string Name = "Jim";
+	public string Description = "";
+	public string ExampleMessages = "";
+}
